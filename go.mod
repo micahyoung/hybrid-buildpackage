e7e590fdfedda6c386aeb961e168a22df974d89f
Micah Young <ymicah@vmware.com>
2020-12-19 07:06:45 -0500

    Initial implementation
    Signed-off-by: Micah Young <ymicah@vmware.com>

---
+++ go.mod
@@ -0,0 +1,8 @@
+module hybrid-image
+
+go 1.15
+
+require (
+	github.com/buildpacks/imgutil v0.0.0-20201211223552-8581300fe2b2
+	github.com/google/go-containerregistry v0.2.1
+)
---
+++ main.go
@@ -0,0 +1,314 @@
+package main
+
+import (
+	"archive/tar"
+	"bytes"
+	"crypto/sha256"
+	"flag"
+	"fmt"
+	"github.com/buildpacks/imgutil/layer"
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/name"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/daemon"
+	"github.com/google/go-containerregistry/pkg/v1/empty"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+var buildpackTomlContent = strings.TrimSpace(`
+api = "0.2"
+
+[buildpack]
+id = "hybrid"
+version = "0.0.1"
+name = "Hybrid OS Buildpack"
+
+[[stacks]]
+id = "io.buildpacks.samples.stacks.nanoserver-1809"
+
+[[stacks]]
+id = "io.buildpacks.samples.stacks.alpine"
+`)
+
+func main() {
+	imageName := flag.String("ref", "", "image ref")
+	publish := flag.Bool("publish", false, "publish to registry")
+	flag.Parse()
+
+	if *imageName == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if err := run(*imageName, *publish); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("image and layer written")
+}
+
+var detectBatContent = strings.TrimSpace(`
+exit 0
+`)
+
+var buildBatContent = strings.TrimSpace(`
+@echo off
+echo Hello windows
+exit 0
+`)
+
+var detectShContent = strings.TrimSpace(`
+#!/bin/sh
+exit 0
+`)
+
+var buildShContent = strings.TrimSpace(`
+#!/bin/sh
+echo Hello linux
+exit 0
+`)
+
+const windowsReadSecurityDescriptor = "AQAAgBQAAAAoAAAAAAAAAAAAAAABAwAAAAAABV0AAAACAAAAAQAAAAEDAAAAAAAFXQAAAAIAAAABAAAA"
+const linuxReadMode = 0777
+
+func run(imageName string, publish bool) error {
+	hybridLayerBlob := &bytes.Buffer{}
+
+	hybridWriter := tar.NewWriter(hybridLayerBlob)
+
+	// write standard windows root directories (require linux permissions)
+	// - needs only Linux permissions as Windows doesn't check grandparent permissions
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "Files",
+		Typeflag: tar.TypeDir,
+		Mode:     linuxReadMode,
+	}); err != nil {
+		return err
+	}
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "Hives",
+		Typeflag: tar.TypeDir,
+	}); err != nil {
+		return err
+	}
+
+	// write buildpack parent-directory hierarchy
+	// - needs only Linux permissions as Windows doesn't check grandparent permissions
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "Files/cnb",
+		Typeflag: tar.TypeDir,
+		Mode:     linuxReadMode,
+	}); err != nil {
+		return err
+	}
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "Files/cnb/buildpacks",
+		Typeflag: tar.TypeDir,
+		Mode:     linuxReadMode,
+	}); err != nil {
+		return err
+	}
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "Files/cnb/buildpacks/hybrid",
+		Typeflag: tar.TypeDir,
+		Mode:     linuxReadMode,
+	}); err != nil {
+		return err
+	}
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "Files/cnb/buildpacks/hybrid/0.0.1",
+		Typeflag: tar.TypeDir,
+		Mode:     linuxReadMode,
+	}); err != nil {
+		return err
+	}
+
+	// write buildpack.toml
+	// - needs Windows & Linux permissions
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "Files/cnb/buildpacks/hybrid/0.0.1/buildpack.toml",
+		Typeflag: tar.TypeReg,
+		Size: int64(len(buildpackTomlContent)),
+		Mode:     linuxReadMode,
+		PAXRecords: map[string]string{
+			"MSWINDOWS.rawsd": windowsReadSecurityDescriptor,
+		},
+		Format:   tar.FormatPAX,
+	}); err != nil {
+		return err
+	}
+	if _, err := hybridWriter.Write([]byte(buildpackTomlContent)); err != nil {
+		return err
+	}
+
+	// write Windows buildpack binary dir
+	// - needs Windows & Linux permissions
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "Files/cnb/buildpacks/hybrid/0.0.1/bin",
+		Typeflag: tar.TypeDir,
+		Mode:     777,
+		PAXRecords: map[string]string{
+			"MSWINDOWS.rawsd": windowsReadSecurityDescriptor,
+		},
+		Format:   tar.FormatPAX,
+	}); err != nil {
+		return err
+	}
+
+	// write Windows buildpack binaries
+	// - needs only Windows permissions
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "Files/cnb/buildpacks/hybrid/0.0.1/bin/detect.bat",
+		Typeflag: tar.TypeReg,
+		Size: int64(len(detectBatContent)),
+		PAXRecords: map[string]string{
+			"MSWINDOWS.rawsd": windowsReadSecurityDescriptor,
+		},
+		Format:   tar.FormatPAX,
+	}); err != nil {
+		return err
+	}
+	if _, err := hybridWriter.Write([]byte(detectBatContent)); err != nil {
+		return err
+	}
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "Files/cnb/buildpacks/hybrid/0.0.1/bin/build.bat",
+		Typeflag: tar.TypeReg,
+		Size: int64(len(buildBatContent)),
+		PAXRecords: map[string]string{
+			"MSWINDOWS.rawsd": windowsReadSecurityDescriptor,
+		},
+		Format:   tar.FormatPAX,
+	}); err != nil {
+		return err
+	}
+	if _, err := hybridWriter.Write([]byte(buildBatContent)); err != nil {
+		return err
+	}
+
+	// write Linux buildpack binaries
+	// - needs only Linux permissions
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "Files/cnb/buildpacks/hybrid/0.0.1/bin/detect",
+		Typeflag: tar.TypeReg,
+		Size: int64(len(detectShContent)),
+		Mode:     linuxReadMode,
+	}); err != nil {
+		return err
+	}
+	if _, err := hybridWriter.Write([]byte(detectShContent)); err != nil {
+		return err
+	}
+
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "Files/cnb/buildpacks/hybrid/0.0.1/bin/build",
+		Typeflag: tar.TypeReg,
+		Size: int64(len(buildShContent)),
+		Mode:     linuxReadMode,
+	}); err != nil {
+		return err
+	}
+	if _, err := hybridWriter.Write([]byte(buildShContent)); err != nil {
+		return err
+	}
+
+	// write Linux parent-directory hierarchy
+	// - note relative paths (no leading /)
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "cnb",
+		Typeflag: tar.TypeDir,
+		Mode:     linuxReadMode,
+	}); err != nil {
+		return err
+	}
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "cnb/buildpacks",
+		Typeflag: tar.TypeDir,
+		Mode:     linuxReadMode,
+	}); err != nil {
+		return err
+	}
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "cnb/buildpacks/hybrid",
+		Typeflag: tar.TypeDir,
+		Mode:     linuxReadMode,
+	}); err != nil {
+		return err
+	}
+
+	// symlink Linux buildpack directory to absolute Windows buildpack directory
+	// - note leading / in Linkname
+	if err := hybridWriter.WriteHeader(&tar.Header{
+		Name:     "cnb/buildpacks/hybrid/0.0.1",
+		Linkname: "/Files/cnb/buildpacks/hybrid/0.0.1",
+		Typeflag: tar.TypeSymlink,
+		Mode:     linuxReadMode,
+	}); err != nil {
+		return err
+	}
+
+	sum := sha256.Sum256(hybridLayerBlob.Bytes())
+	shasum := fmt.Sprintf("sha256:%x", sum)
+
+	fmt.Println("building new image")
+	image, err := mutate.ConfigFile(empty.Image, &v1.ConfigFile{
+		Config: v1.Config{
+			Labels: map[string]string{
+				"io.buildpacks.buildpack.layers":      `{"hybrid":{"0.0.1":{"api":"0.2","stacks":[{"id":"io.buildpacks.samples.stacks.alpine"},{"id":"io.buildpacks.samples.stacks.nanoserver-1809"}],"layerDiffID":"` + shasum + `"}}}`,
+				"io.buildpacks.buildpackage.metadata": `{"id":"hybrid","version":"0.0.1","stacks":[{"id":"io.buildpacks.samples.stacks.alpine"},{"id":"io.buildpacks.samples.stacks.nanoserver-1809"}]}`,
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	hybridLayer, err := tarball.LayerFromReader(hybridLayerBlob)
+	if err != nil {
+		return err
+	}
+
+	// generate Windows-scratch base layer
+	windowsBaseLayerReader, err := layer.WindowsBaseLayer()
+	if err != nil {
+		return err
+	}
+
+	windowsScratchBaseLayer, err := tarball.LayerFromReader(windowsBaseLayerReader)
+	if err != nil {
+		return err
+	}
+
+	image, err = mutate.AppendLayers(image, windowsScratchBaseLayer, hybridLayer)
+	if err != nil {
+		return err
+	}
+
+	tag, err := name.NewTag(imageName)
+	if err != nil {
+		return err
+	}
+
+	if publish {
+		if err := remote.Write(tag, image, remote.WithAuthFromKeychain(authn.DefaultKeychain)); err != nil {
+			return err
+		}
+	} else {
+		fmt.Println("writing new image")
+		output, err := daemon.Write(tag, image)
+		if err != nil {
+			return err
+		}
+		if _, err := io.Copy(os.Stderr, bytes.NewBuffer([]byte(output))); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
